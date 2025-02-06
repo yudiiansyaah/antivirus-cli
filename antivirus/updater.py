ae4a7902cd419ab8b11f668e56e0ba79340ef05f
MY <129849045+yudiiansyaah@users.noreply.github.com>
2025-02-06 18:11:39 +0700

    Update updater.py

--- antivirus/updater.py
+++ antivirus/updater.py
@@ -1,13 +1,6 @@
 import urllib.request
 
 def update_virus_definitions(virus_defs_url, local_path="virus_definitions.txt"):
-    """
-    Update the local virus definitions by downloading from the provided URL.
-    
-    Args:
-        virus_defs_url (str): URL from which to download the virus definitions.
-        local_path (str): Path to save the downloaded virus definitions (default is 'virus_definitions.txt').
-    """
     try:
         print("Downloading virus definitions...")
         urllib.request.urlretrieve(virus_defs_url, local_path)
